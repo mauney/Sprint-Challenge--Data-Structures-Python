94b5a74aa28108cc448015f5b65c3368e814cb57
Matthew Mauney <mauney@users.noreply.github.com>
2020-01-31 13:23:00 -0500

    Complete names stretch

--- names/names.py
+++ names/names.py
@@ -43,3 +43,19 @@ we are running n searches. Total time complexity is O(n log(n)).
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish with no restrictions on techniques or data
 # structures?
+
+start_time = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+duplicates_set = set(names_1).intersection(set(names_2))
+
+end_time = time.time()
+print (f"{len(duplicates_set)} duplicates_set:\n\n{', '.join(duplicates_set)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
